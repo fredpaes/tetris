6b5acc258f7dbd66108c6bd0f914450d8d3dadd6
Fred Paucar <fred.paucarespinoza@gmail.com>
2020-07-11 13:36:41 -0500

    add translation

--- lib/brick.ex
+++ lib/brick.ex
@@ -6,16 +6,34 @@ defmodule Tetris.Brick do
     reflection: false
   ]
 
-  def new, do: __struct__()
+  def new(attributes \\ []), do: __struct__()
 
   def new_random do
-    %{
+    %__MODULE__{
       name: random_name(),
       location: {40, 0},
       rotation: random_rotation(),
       reflection: random_reflection()
     }
   end
+
+  def right(brick) do
+    %{brick | location: point_right(brick.location)}
+  end
+
+  def left(brick) do
+    %{brick | location: point_left(brick.location)}
+  end
+
+  def down(brick) do
+    %{brick | location: point_down(brick.location)}
+  end
+
+  def spin(brick) do
+    %{brick | rotation: rotate(brick.rotation)}
+  end
+
+
 
   def random_name do
     ~w(i l z o t)a
@@ -30,5 +48,56 @@ defmodule Tetris.Brick do
   def random_reflection do
     [true, false]
     |> Enum.random
+  end
+
+
+  def point_right({x, y}) do
+    {x+1, y}
+  end
+
+  def point_left({x, y}) do
+    {x-1, y}
+  end
+  
+  def point_down({x, y}) do
+    {x, y+1}
+  end
+
+  def rotate(degrees) do
+    case degrees do
+      270 -> 0
+      _ -> degrees + 90
+    end
+  end
+
+
+  def shape(%{name: letter}) do
+    case letter do
+      :i -> [
+        {2, 1},
+        {2, 2},
+        {2, 3},
+        {2, 4}
+      ]
+      :l -> [
+        {2, 1},
+        {2, 2},
+        {2, 3}, {3, 3}
+      ]
+      :z -> [
+        {2, 1},
+        {2, 2}, {3, 2},
+                {3, 3}
+      ]
+      :o -> [
+        {2, 1}, {3, 1},
+        {2, 2}, {3, 2}
+      ]
+      :t -> [
+        {2, 1},
+        {2, 2}, {3, 2},
+        {2, 3}
+      ]
+    end
   end
 end
--- test/brick_test.exs
+++ test/brick_test.exs
@@ -2,6 +2,7 @@ defmodule BrickTest do
   use ExUnit.Case
 
   import Tetris.Brick
+  import Tetris.Points
 
   test "Creates a new brick" do
     assert new_brick.name() == :i
@@ -11,7 +12,39 @@ defmodule BrickTest do
     actual = new_random()
 
     assert actual.name in [:i, :l, :z, :t, :o]
+    assert actual.rotation in [0, 90, 180, 270]
+    assert actual.reflection in [true, false]
   end
 
-  def new_brick, do: new()
+  test "Should manipulate brick" do
+    actual = new_brick()
+      |> left
+      |> right
+      |> right
+      |> down
+      |> spin
+      |> spin
+
+    assert actual.location == {41, 1}
+    assert actual.rotation == 180
+  end
+
+  test "Should return points for z shape" do
+    points_brick = new_brick(name: :z)
+      |> shape
+
+    assert {3, 3} not in points_brick
+  end
+
+  test "should translate a list of points" do
+    actual_points = new_brick()
+      |> shape
+      |> translate({1, 1})
+      |> translate({0, 1})
+
+    assert actual_points !== []
+  end
+
+  # method to create a randow brick
+  def new_brick(attributes \\ []), do: new(attributes)
 end
